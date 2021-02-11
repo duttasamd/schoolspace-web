dc9be46c7be02a0c34f74f66889caecf355e00c2
Samrat Dutta <dutta.sam.d@gmail.com>
2021-02-11 04:19:47 +0100

    Updates

---
+++ precache-manifest.0388049a99fcedd61b1fd0c3c3585642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51f075013c706ddf8360a808f2026fe",
+    "url": "http://demo.schoolspace.co.in/index.html"
+  },
+  {
+    "revision": "8aee035a79ab2d09882c",
+    "url": "http://demo.schoolspace.co.in/static/css/2.bf9db4fe.chunk.css"
+  },
+  {
+    "revision": "2a3336b33a70d8466e4c",
+    "url": "http://demo.schoolspace.co.in/static/css/main.d1b29013.chunk.css"
+  },
+  {
+    "revision": "8aee035a79ab2d09882c",
+    "url": "http://demo.schoolspace.co.in/static/js/2.62f9ddd7.chunk.js"
+  },
+  {
+    "revision": "6497a67d0d210861fddb8c6f16aa1769",
+    "url": "http://demo.schoolspace.co.in/static/js/2.62f9ddd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3336b33a70d8466e4c",
+    "url": "http://demo.schoolspace.co.in/static/js/main.05c78e4e.chunk.js"
+  },
+  {
+    "revision": "78ac183c9a00d277b286",
+    "url": "http://demo.schoolspace.co.in/static/js/runtime-main.78aff0a9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://demo.schoolspace.co.in/precache-manifest.bd3b2765184ce153d7b8ad6a47996ade.js"
+  "http://demo.schoolspace.co.in/precache-manifest.0388049a99fcedd61b1fd0c3c3585642.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://demo.schoolspace.co.in/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://demo.schoolspace.co.in/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
