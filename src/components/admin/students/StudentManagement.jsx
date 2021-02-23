36bc02f7e28161eadd3b073425dfe896f7d6de0a
Suman Das <sum4ndas@gmail.com>
2021-02-23 17:00:28 +0530

    assignment page

--- src/components/admin/students/StudentManagement.jsx
+++ src/components/admin/students/StudentManagement.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Assignments from "../../assignments/Assignments";
 import StudentTable from "./StudentTable";
 
 class StudentManagement extends Component {
   render() {
     return (
       <div>
+        <Assignments />
         <StudentTable />
       </div>
     );
---
+++ src/components/assignments/Assignments.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextEditor from '../tools/TextEditor';
+
+function Assignments() {
+    return (
+        <div classname="container">
+            <h1 className="text-center">Assignment Title</h1>
+            <h3 className="text-center">Assignment Description</h3>
+            <TextEditor />
+            <button className="btn btn-primary btn-lg">Submit</button>
+        </div>
+    )
+}
+
+export default Assignments
